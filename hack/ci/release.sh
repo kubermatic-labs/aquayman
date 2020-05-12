456e040911fa8eb53b26017f4ea4b20967a752ba
Christoph Mewes <christoph@loodse.com>
2020-05-12 20:57:25 +0200

    login to docker registry before building images

--- hack/ci/release.sh
+++ hack/ci/release.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
+#
+# This script is creating release binaries and
+# Docker images via goreleaser. It's meant to
+# run in the Kubermatic CI environment only.
 
 set -euo pipefail
 
 cd $(dirname $0)/../..
 
 git remote add origin git@github.com:kubermatic-labs/aquayman.git
+docker login -u "$QUAY_IO_USERNAME" -p "$QUAY_IO_PASSWORD" quay.io
 
 export GITHUB_TOKEN=$(cat /etc/github/oauth | tr -d '\n')
 goreleaser release
