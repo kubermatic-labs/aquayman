494336cb5fdb3d03a70b405847ef5078dfd81381
Christoph Mewes <christoph@loodse.com>
2020-05-13 11:47:07 +0200

    fix missing github token

--- hack/ci/release.sh
+++ hack/ci/release.sh
@@ -10,4 +10,6 @@ set -euo pipefail
 cd $(dirname $0)/../..
 
 git remote add origin git@github.com:kubermatic-labs/aquayman.git
+export GITHUB_TOKEN=$(cat /etc/github/oauth | tr -d '\n')
+
 goreleaser release
